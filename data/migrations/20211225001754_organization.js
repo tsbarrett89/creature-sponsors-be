d76b7d85013f8dd1b0523d13cc3e580cf19e5c12
tsbarrett89 <tsbarrett89@gmail.com>
2021-12-24 17:26:57 -0700

    organization table created and dropped

--- data/migrations/20211225001754_organization.js
+++ data/migrations/20211225001754_organization.js
@@ -1,8 +1,15 @@
 
 exports.up = function(knex) {
-  
+    return knex.schema.createTable('organization', tbl => {
+        tbl.increments();
+        tbl.string('email')
+            .notNullable()
+            .unique();
+        tbl.string('password')
+            .notNullable();
+    })
 };
 
 exports.down = function(knex) {
-  
+    return knex.schema.dropTableIfExists('organization');
 };

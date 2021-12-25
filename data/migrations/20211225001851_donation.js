27bdbf54b90e8838ca9f3c770f075cf398e62575
tsbarrett89 <tsbarrett89@gmail.com>
2021-12-24 17:44:41 -0700

    donation table created and dropped

--- data/migrations/20211225001851_donation.js
+++ data/migrations/20211225001851_donation.js
@@ -1,8 +1,27 @@
 
 exports.up = function(knex) {
-  
+    return knex.schema.createTable('donation', tbl => {
+        tbl.increments();
+        tbl.integer('campaign_id')
+            .unsigned()
+            .notNullable()
+            .references('id')
+            .inTable('campaign')
+            .onDelete('RESTRICT')
+            .onUpdate('CASCADE');
+        tbl.integer('user_id')
+            .unsigned()
+            .notNullable()
+            .references('id')
+            .inTable('user')
+            .onDelete('RESTRICT')
+            .onUpdate('CASCADE')
+        tbl.integer('amount')
+            .unsigned()
+            .notNullable()
+    })
 };
 
 exports.down = function(knex) {
-  
+    return knex.schema.dropTableIfExists('donation')
 };

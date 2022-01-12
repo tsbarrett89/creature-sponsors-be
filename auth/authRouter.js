e2e553daaf6f4cffeb5eba6f3957caf01d127a7d
tsbarrett89 <tsbarrett89@gmail.com>
2022-01-12 13:43:56 -0700

    creds declared, if else statement to check for creds

--- auth/authRouter.js
+++ auth/authRouter.js
@@ -5,3 +5,18 @@ const user = require('../user/userModel');
 const org = require('../organization/organizationModel');
 const router = express.Router();
 
+router.post('/register/user', (req, res) => {
+    const creds = req.body;
+
+    if (creds.email && creds.password){
+        const hash = bcrypt.hashSync(creds.password, 12);
+        creds.password = hash;
+        
+        user.add(creds)
+            .then(newUser => {
+                
+            })
+    } else {
+        res.status(400).json({ errorMessage: "Email and password are required to register."})
+    }
+})

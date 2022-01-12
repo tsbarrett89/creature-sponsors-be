28b0067e7b9cdb5ed93e79b64bb57a5e460b4ba6
tsbarrett89 <tsbarrett89@gmail.com>
2022-01-12 13:55:05 -0700

    authRouter imported and used in server

--- api/server.js
+++ api/server.js
@@ -1,9 +1,15 @@
 const express = require('express');
 
+const authRouter = require('../auth/authRouter.js')
+
 const server = express();
+
+
 
 server.get('/', (req, res) => {
     res.send('Critter Sponsor API')
 });
+
+server.use('/auth', authRouter)
 
 module.exports = server;

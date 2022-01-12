5e5f2e43e53a750875814aee91f982ffd99b3cd2
tsbarrett89 <tsbarrett89@gmail.com>
2022-01-12 13:56:48 -0700

    helmet and cors imported and used in server

--- api/server.js
+++ api/server.js
@@ -1,10 +1,14 @@
 const express = require('express');
+const helmet = require('helmet');
+const cors = require('cors');
 
 const authRouter = require('../auth/authRouter.js')
 
 const server = express();
 
-
+server.use(helmet());
+server.use(express.json());
+server.use(cors());
 
 server.get('/', (req, res) => {
     res.send('Critter Sponsor API')

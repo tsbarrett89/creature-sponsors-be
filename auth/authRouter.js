4e7ec4d8e6fc27104a3a0a41bf4d107e30692759
tsbarrett89 <tsbarrett89@gmail.com>
2022-02-09 17:41:04 -0700

    adjusted generateToken

--- auth/authRouter.js
+++ auth/authRouter.js
@@ -14,7 +14,8 @@ router.post('/register/user', (req, res) => {
         
         user.add(creds)
             .then(newUser => {
-                const token = generateToken(newUser[0])
+                const token = generateToken(newUser)
+                console.log(token)
 
                 res.status(201).json({ email: creds.email, token, user_id: newUser[0]});
             })
